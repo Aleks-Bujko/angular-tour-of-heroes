42009f4588afc8aac4b5f4ebc36428bbe90813f8
Aleks-Bujko <78426734+Aleks-Bujko@users.noreply.github.com>
2021-02-04 13:35:51 +0100

    Fixed app.module

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -1,10 +1,11 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { AppRoutingModule } from './app-routing.module';
+
 
 @NgModule({
   declarations: [
